62b6adbf6160d1f48af6ddb165baad9df5cb8a02
Miłosz Jura <milosz.m.jura@gmail.com>
2024-08-14 20:46:26 +0200

    Added scraper

---
+++ fb.py
@@ -0,0 +1,218 @@
+from Browser import Browser
+import traceback
+import sys
+import configparser
+import json
+from time import sleep
+from random import randint
+import logging 
+from datetime import datetime
+import pathlib
+from src.sqlite3_connector import SqLite3Connector
+
+logger = logging.getLogger(__name__)
+
+
+
+class L:
+    # Login
+    accept = """(//span[contains(text(),"Zezwól na wszystkie pliki cookie") or contains(text(),"Allow all cookies")])[2]"""
+    mail = """//input[@id="email"]"""
+    password = """//input[@id="pass"]"""
+    login_btn = """//button[@name="login"]"""
+
+    # page
+    post_div = """//*[@data-ad-preview="message"]"""
+    show_more = post_div + """//div[text()="Wyświetl więcej" or text()="Show more"]"""
+
+cp = configparser.ConfigParser()
+cp.read(".config.ini")
+
+
+class MyBrowser(Browser):
+    fb_url = "https://facebook.com"
+
+    def __init__(self, start_path, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start_path = pathlib.Path(start_path)
+        self.urls= json.load(open(pathlib.Path(self.start_path, "urls.json"),"r"))
+        self.console = False
+        self.debug = kwargs.get("--debug")
+        self.db = SqLite3Connector(database=pathlib.Path(self.start_path, "fb_scrapper.db"))
+        try:
+            self.headless_setting = bool(int(cp["BROWSER"]["headless"]))
+        except:
+            self.headless_setting = True
+        try:
+            self.print_name = bool(cp["FB"]["print_name"])
+        except:
+            self.print_name = False
+        self.register_keyword_to_run_on_failure("take_screenshot")
+        self.log_file_name = f"fb_log_{self.get_timestamp_file()}.log"
+        self.log_file_path = pathlib.Path(self.start_path, self.log_file_name)
+        self.output_file_name = f"fb_output_{self.get_timestamp_file()}.log"
+
+    def open_fb(self):
+        self.new_browser(headless=self.headless_setting)
+        self.new_page(self.fb_url)
+        self.wait_for_elements_state(L.accept)
+        self.sleep_random(3)
+        self.slow_click(L.accept)
+        self.wait_for_elements_state(L.mail)
+        self.sleep_random(2)
+        self.save_to_log("Opened Facebook with success", console=self.console)
+
+    def login(self, credentials:dict):
+        self.slow_type_text(L.mail, credentials["email"])
+        self.slow_type_text(L.password, credentials["password"])
+        self.slow_click(L.login_btn)
+        self.wait_for_elements_state(f"""//span[contains(text(),"{credentials['fullname']}")]""", timeout=100)
+        self.save_to_log(f"Logged successfully{' as ' + credentials['fullname'] if self.print_name else ''}")
+
+    def scrap_from_ulrs(self):
+        if not len(self.urls):
+            self.save_to_log("Urls were empty, no input to scrap!")
+        for k,v in self.urls.items():
+            try:
+                self.scrap_page(k,v)
+            except Exception as e:
+                self.save_to_log(f"ERROR: could not scrap '{k}' due to {type(e).__name__}: {traceback.format_exc()}")
+
+    def scrap_page(self, name, url, max_posts:int=10):
+        # get last post to scrap
+        # set locators
+        page_name_locator = f"""//h1[text()="{name}"]"""
+        # go to the page
+        self.save_to_log(f"Starting scrap: '{name}'", console=self.console)
+        self.go_to(url)
+        self.wait_for_elements_state(page_name_locator, timeout=100, message=f"Could not verify that '{name}' was opened as '{page_name_locator}' was not found")
+        # scrap data
+        i = 0
+        while i <= max_posts:
+            print(f"\t- post number {i}")
+            i += 1
+            # set post locator
+            locator = f"({L.post_div})[{i}]"
+            # get post data
+            try:
+                try:
+                    for e in self.get_elements(L.show_more):
+                        self.slow_click(e)
+                except Exception as e:
+                    print(traceback.format_exc())
+                    traceback.print_exc(file=sys.stdout)
+                    self.save_to_log(f"WARNING - {name} - {i} - could not click show more due to {e}",console=False)
+                content = self.get_text(locator)
+            except:
+            # scroll to bottom if needed
+                self.scroll_to_bottom()
+                try:
+                    content = self.get_text(locator)
+                except:
+                    self.save_to_log(f"Could not get content, closing ar {i}")
+                    break
+            # check if post already exists
+            post_exists = not bool(len(self.fetch_post_by_first_40(name, content[:40])[0]))
+            try:
+                #self.save_to_log("last_scrapped:\n"+str(last_scrapped[0][0]).lower()+"\n-x-x-x-\n"+ str(content[:40]).lower() + ".end", console=True)
+                if post_exists:
+                    # break if exsits
+                    self.save_to_log(f"{name} - {i} - post already in db, breaking.", console=self.console)
+                    break
+            except IndexError:
+                self.save_to_log(f"WARNING {name} - {i} - could not be compared with latest posts", console=self.console)
+            # save 
+            now_date = self.get_timestamp_post()
+            if self.debug:
+                self.save_to_log(f"DEBUG {name} - {i} - name: {name}, date: {now_date}\n\tcontent: {content}")
+            self.insert_scrapped_post(
+                    page_name=name,
+                    date=now_date,
+                    post_content=content
+                    )
+        self.save_to_log(f"Scrapped {i-1} posts from {name}")
+
+    def scroll_to_bottom(self):
+        self.evaluate_javascript(None, "window.scrollTo(0, document.body.scrollHeight);")
+        self.sleep_random(3)
+
+    def fetch_post_by_first_40(self, name:str, first_40:str):
+        query = (
+            'SELECT id'
+            'FROM scraps '
+            f'WHERE page_name=(?) '
+            f'AND post_first_40=(?) '
+            'ORDER BY id DESC '
+            'LIMIT 1'
+            )
+        args = (name, first_40)
+        return self.db.execute(query, *args)
+
+    def insert_scrapped_post(self, page_name:str, date:str, post_content:str):
+        query = (
+                'INSERT INTO scraps '
+                '(page_name, date, post_content, post_first_40, notification_sent) '
+                f'VALUES ((?), (?), (?), (?), 0)'
+                )
+        args = (page_name, date, post_content, post_content[:40])
+        result = self.db.execute(query, *args)
+        try:
+            if "ERROR" in result[0][0]:
+                self.save_to_log(f"ERROR: {page_name} - could not save post due to {result[0][0]}")
+        except IndexError:
+            pass
+    
+    def slow_click(self, selector:str, timeout:int=1):
+        self.click(selector)
+        self.sleep_random(timeout)
+
+    def slow_type_text(self, selector:str, text:str, timeout:int=1):
+        self.click(selector)
+        self.type_text(selector, text)
+        self.sleep_random(timeout)
+
+    def sleep_random(self, base_timeout:int=1):
+        r"Sleeps random time based on base_timeout +/- 100% of time"
+        base_time = base_timeout * 100
+        time_to_sleep = (randint(1,base_time))/100
+        sleep(time_to_sleep)
+
+    def save_to_log(self, msg, console:bool=True):
+        with open(self.log_file_path, "a") as f:
+            f.write(self.get_timestamp_log() + " " + str(msg) + "\n")
+            print(f"wrote to {self.log_file_path}")
+        if console:
+            print(msg)
+
+    def get_timestamp_post(self):
+        return f"{datetime.now().strftime('%y-%m-%d %H:%M')}"
+
+    def get_timestamp_file(self):
+        return f"{datetime.now().strftime('%y_%m_%d_%H_%M')}"
+
+    def get_timestamp_log(self):
+        return f"[{datetime.now().strftime('%y-%m-%d %H:%M')}]"
+
+
+if __name__ == "__main__":
+    if "--email" in sys.argv\
+    and "--password" in sys.argv\
+    and "--fullname" in sys.argv\
+    and "--startpath" in sys.argv:
+        login_creds = {
+                "email": sys.argv[sys.argv.index("--email")+1],
+                "password": sys.argv[sys.argv.index("--password")+1],
+                "fullname": sys.argv[sys.argv.index("--fullname")+1],
+                }
+        start_path = sys.argv[sys.argv.index("--startpath") + 1]
+    else:
+        try:
+            login_creds = json.load(open("fb_creds.config.json", "r"))
+            start_path = login_creds["start_path"]
+        except:
+            raise FileNotFoundError("Could not find 'fb_creds.config.json file or it is not JSON seriazable!'")
+    b = MyBrowser(start_path = start_path)
+    b.open_fb()
+    b.login(login_creds)
+    b.scrap_from_ulrs()
+
---
+++ notifications.py
@@ -0,0 +1,166 @@
+from src.sqlite3_connector import SqLite3Connector
+import pathlib
+import sys
+from time import time
+import os
+
+help_text ="""notification creator for fb_scrapper
+    -i[execution_id]    - set execution_id manually
+    -c[content_type]    - content type
+    -h   --help         - print help
+
+    example:
+    notifications.py -itest_1 -chtml  <- create notificaiton_test1.txt file with html formatting
+"""
+
+class NotificaitonGenerator:
+    extensions = {
+            "plain": "txt",
+            "html": "html"
+            }
+    formatting = {
+            "plain":{
+                "hs": "=== ",
+                "he": "",
+                "h2s": "-- ",
+                "h2e": "-- ",
+                "be": "",
+                "bs": "",
+                "break": "------------------------------------------",
+                "post_start": "",
+                "post_end": "",
+                "content_start": "",
+                "content_end": "",
+                "msg_start": "",
+                "msg_end": "",
+                },
+            "html":{
+                "msg_start": """<!doctype html><html><body style="background: #f6f4f2;font-family:'Courier New';color:#263238;">""",
+                "msg_end": "</body></html>",
+                "hs": """<span style="background: #FFFFFF;"><h1 style="display:block;text-align:center;margin-left:40px;margin-top: 20px;margin-right:40px;margin-bottom:10px;">""",
+                "he": "</h1></span>",
+                "subhs": """<span style="text-align:center;display:block;">""",
+                "subhe": "</span>",
+                "h2s": """<h2 style="padding-top: 10px;">""",
+                "h2e": "</h2>",
+                "bs": "<b>",
+                "be": "</b>",
+                "break": """<hr style="margin-left: 50px;margin-top: 30px;margin-bottom:20px;margin-right: 50px;">""",
+                "post_start": """<div style="background: #FFFFFF;padding-left: 40px;padding-right: 40px;padding-bottom:40px;margin-left: auto;margin-right:auto;width:50%;text-align:justify;margin-bottom:40px;">""",
+                "post_end": """</div>""",
+                "content_start": """<span style="margin-left: 50px;margin-right:50px;margin-bottom:40px;">""",
+                "content_end": """</span>""",
+                }
+            }
+
+    def __init__(self, execution_id:str | None = None, starting_path:str=""):
+        self.path = pathlib.Path(starting_path)
+        self.db = SqLite3Connector(pathlib.Path(self.path, "fb_scrapper.db"))
+        self.execution_id = execution_id if execution_id is not None else int(time())
+
+    def fetch_all_unsent(self):
+        # get all unsent
+        query = (
+                "SELECT page_name, date, post_content "
+                "FROM scraps "
+                "WHERE notification_sent=0 "
+                "ORDER BY id DESC"
+                )
+        results = self.db.execute(query)[0]
+        with open(pathlib.Path(self.path, "notificationx.log"),"a")as f:
+            for i, q in enumerate(results):
+                f.write(f"==={i} Query\n{q}\n\n")
+        return results
+    
+    def mark_all_sent(self):
+        query = (
+                "UPDATE scraps "
+                "SET notification_sent = 1 "
+                "WHERE notification_sent = 0"
+                )
+        self.db.execute(query)
+
+    def prepare_notification(self, format_type:str):
+        file_extension = self.extensions.get(content_type)
+        file_extension = "txt" if file_extension is None else file_extension
+        if format_type not in self.formatting.keys():
+            raise KeyError(f"{format_type} not in {self.formatting.keys()}!")
+        f = self.formatting[format_type]
+        results = self.fetch_all_unsent()
+        posts = {}
+        for result in results:
+            posts[result[0]] = []
+
+        for result in results:
+            this_post = {
+                        "date": result[1],
+                        "content": result[2],
+                        }
+            posts[result[0]].append(this_post)
+        message = f"{f['msg_start']}"
+        for k,v in posts.items():
+            message += f"{f['hs']}{f['bs']}{k}{f['be']}{f['he']}\n{f['subhs']}{len(v)} new post{'s' if len(v) > 1 else ''}{f['subhe']}\n"
+            for row in v:
+                message += f['post_start'] + f['h2s'] + row["date"] + f["h2e"] + "\n"
+                first_sentence_b_msg = self.bold_first_sentence(row["content"], format_type)
+                message +=  f["content_start"] + first_sentence_b_msg + "\n\n\n" + f["content_end"] + f["post_end"]
+            message += f['break']
+        # replace \n with <br> for HTML
+        if format_type == "html":
+            message.replace("\n", "<br>")
+        message += f["msg_end"]
+        if len(message) <= (len(f["msg_end"]) + len(f["msg_start"])):
+            message = self.return_template("empty", content_type)
+        with open(pathlib.Path(self.path, f"notification_{self.execution_id}.{file_extension}"), "w") as f:
+            f.write(message)
+        self.mark_all_sent()
+
+    def bold_first_sentence(self, msg:str, content_type) ->str:
+        sentence_finishers = [".", "?", "!"]
+        #finisher_places = {}
+        # setup formatting
+        f = self.formatting[content_type]
+        min_sf = 1000000
+        first_sf= ""
+        # find first finihser
+        for sf in sentence_finishers:
+            #finisher_places[sf] = msg.find(sf)
+            sf_place = str(msg).find(sf)
+            if sf_place < 0:
+                continue
+            min_sf = min(min_sf, sf_place)
+            if min_sf == sf_place:
+                first_sf = sf
+        if not first_sf:
+            return msg
+        msg = f['bs'] + msg.replace(first_sf, first_sf + f['be'], 1)
+        # add bold end at the end if needed
+        #msg = msg if f["be"] in msg else msg + f["be"]
+        return msg 
+
+    def return_template(self, template:str, content_type:str) -> str:
+        f = self.formatting[content_type]
+        templates = {
+            "empty": f"""{f["msg_start"]}{f["hs"]}There were 0 new posts from your pages{f["he"]}
+Have a great day!{f["msg_end"]}"""
+                }
+        return templates[template]
+
+if __name__ == "__main__":
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print(help_text)
+        exit()
+    exc_id = None
+    content_type = "plain"
+    starting_path = os.path.abspath(os.path.dirname(__file__))
+    for arg in sys.argv:
+        if arg.startswith("-i"):
+            exc_id = arg[2:]
+            continue
+        if arg.startswith("-c"):
+            content_type = arg[2:]
+        if arg.startswith("-d"):
+            starting_path = arg[2:]
+    n = NotificaitonGenerator(execution_id=exc_id, starting_path=starting_path)
+    n.prepare_notification(content_type)
+
